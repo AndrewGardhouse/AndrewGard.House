faef91a528d43ecd374d1f8238ad2399b14a33bc
AndrewGardhouse <AndrewGardhouse@gmail.com>
2015-03-08 15:29:11 -0700

    updated portfolio

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+    
+    $('.parallax').parallax();
+
+    var options = [
+      {selector: '#tech-skills', offset: 100, callback: 'showStaggeredList("#frontend-tech")' },
+      {selector: '#backend-tech', offset: 0, callback: 'showStaggeredList("#backend-tech")' },
+      {selector: '#code', offset: 150, callback: 'fadeInImage("#code")' },
+      {selector: '#threedb', offset: 225, callback: "fadeInImage('#threedb')" },
+    ];
+    scrollFire(options);
+
+  });
+
+  $(function(){
+
+    $('.modal-trigger').leanModal();
+  });
+})(jQuery); // end of jQuery name space
