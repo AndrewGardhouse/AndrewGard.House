e00b58b88273ba1f957dfea064b3ebdaa6fcc963
AndrewGardhouse <AndrewGardhouse@gmail.com>
2016-01-07 16:08:56 -0800

    Changed form colour to white, added links to form and back to site

--- js/init.js
+++ js/init.js
@@ -27,6 +27,13 @@
     scrollFire(options);
 
     $('.modal-trigger').leanModal();
+
+    $('a.contact-me').click(function(){
+      $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 2500);
+      return false;
+    });
   });
 
 })(jQuery);
