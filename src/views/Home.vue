4922e343018d5b1e4f433710840041086dab6862
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-07-11 17:56:09 -0400

    Adds fade-in transition on title and options

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,63 @@
 <template lang="html">
   <div class="container">
-    <div class="title">
-      <h1>ANDREW GARDHOUSE</h1>
-      <p>Web Developer</p>
-    </div>
-    <div class="options">
-      <ul>
-        <li>
-          <router-link to="/about">
-            <p>
-              About
-            </p>
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/about">
-            <p>
-              Get In Touch
-            </p>
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/about">
-            <p>
-              Resume
-            </p>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+    <transition name="title-fade" appear>
+      <div class="title">
+        <h1>ANDREW GARDHOUSE</h1>
+        <p>Web Developer</p>
+      </div>
+    </transition>
+    <transition name="options-fade" appear>
+      <div class="options">
+        <ul>
+          <li>
+            <router-link to="/about">
+              <p>
+                About
+              </p>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/about">
+              <p>
+                Get In Touch
+              </p>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/about">
+              <p>
+                Resume
+              </p>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
 
 <style lang="less">
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.title-fade-enter-active, .title-fade-leave-active {
+  opacity: 0;
+  animation: fadein 2s linear;
+  animation-delay: 2s;
+}
+
+.options-fade-enter-active, .options-fade-leave-active {
+  opacity: 0;
+  animation: fadein 0.5s linear;
+  animation-delay: 3s;
+}
+
 .title, .options {
   margin-left: auto;
   margin-right: auto;
@@ -41,7 +66,7 @@
 .title {
   margin-top: auto;
   h1 {
-    font-size: 6rem;
+    font-size: 4rem;
     margin-bottom: 0;
   }
   p {
@@ -49,6 +74,7 @@
     margin-top: 1rem;
   }
 }
+
 .options {
   margin-top: 1.5rem;
   margin-bottom: auto;
