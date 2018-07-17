8c5c6f22119b70dac7c40369337ad9bf604c6de3
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-07-17 13:54:12 -0400

    Adds link back to home in about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link to="/">
+      <p>
+        Home
+      </p>
+    </router-link>
   </div>
 </template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+</style>
