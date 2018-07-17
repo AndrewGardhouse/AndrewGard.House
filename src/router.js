f1b8e8b56a88bad78bafd4572dad683c123cb252
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-07-17 13:52:40 -0400

    Adds contact form view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Contact from './views/Contact.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
   ]
 })
---
+++ src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+    <h1>Get In Touch</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+</style>
