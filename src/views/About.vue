cc8fb8a881227b677763cbbbc458e2b18fc20b1c
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-08-09 15:06:13 -0400

    Adds a scene with a walking character to the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <router-link to="/">
-      <p>
-        Home
-      </p>
-    </router-link>
+    <transition name="about-page-loaded" appear>
+      <div class="scene">
+        <Character></Character>
+        <div class="ground"></div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import Character from '../components/Character'
+
 export default {
+  components: {
+    Character
+  }
 }
 </script>
 
 <style lang="less">
+@keyframes about-page-loaded {
+  from {
+    top: 40%;
+  }
+  to {
+    top: 0%;
+  }
+}
+
+.about-page-loaded-enter-active {
+  animation: about-page-loaded 2.5s linear;
+}
+
+.about {
+  height: 100vh;
+  position: relative;
+  z-index: 2;
+  .scene {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    height: 100%;
+    top: 0%;
+    .ground {
+      height: 25%;
+      background-color: #000;
+    }
+  }
+}
 </style>
