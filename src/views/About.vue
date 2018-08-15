04dfc38872b0a6a387b7cbb01efca0a4456d850d
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-08-15 14:36:48 -0400

    Adds buttons for skill cards and home page navigation

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,13 @@
   <div class="about">
     <transition name="about-page-loaded" appear>
       <div class="scene">
-        <div class="max-width-4 mx-auto inline-block relative">
+        <div class="max-width-4 mx-auto info inline-block relative">
           <p class="h2 pb4">After that bit of shenanigans, Cool and I looked at buying another vendor set for my Shadow spec but I was having a lot of crashing issues as soon as I ported from Org to the Valley. </p>
+          <div class="skills flex mb3">
+            <button class="button mx3 first">Front-End</button>
+            <button class="button mx3 second">Back-End</button>
+            <router-link to="/" class="button mx3 third">Home</router-link>
+          </div>
         </div>
         <Character></Character>
         <div class="ground"></div>
@@ -39,16 +44,40 @@ export default {
     justify-content: flex-end;
     height: 100%;
     top: 0%;
-    p {
-      line-height: 1.5;
-      overflow: hidden;
-      padding: 0;
-      opacity: 0;
-      animation: text-scroll;
-      animation-timing-function: linear;
-      animation-duration: 6s;
-      animation-fill-mode: forwards;
-      animation-delay: 3s;
+    .info {
+      p {
+        line-height: 1.5;
+        overflow: hidden;
+        padding: 0;
+        opacity: 0;
+        animation: text-scroll;
+        animation-timing-function: linear;
+        animation-duration: 3s;
+        animation-fill-mode: forwards;
+        animation-delay: 1s;
+      }
+      .skills {
+        justify-content: center;
+        flex: 1 1 0;
+        .button {
+          animation: fadein;
+          animation-timing-function: linear;
+          animation-duration: .75s;
+          opacity: 0;
+          animation-fill-mode: forwards;
+          flex-grow: 1;
+          flex-basis: 0;
+          &.first {
+            animation-delay: 3.25s;
+          }
+          &.second {
+            animation-delay: 3.75s;
+          }
+          &.third {
+            animation-delay: 4.25s;
+          }
+        }
+      }
     }
     .ground {
       height: 25%;
