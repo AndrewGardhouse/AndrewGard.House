b6e9bf919e2ab2092f3dc718668fd80dabb8ff77
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-08-20 17:23:26 -0400

    Adds vue-analytics to project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: ' UA-58720312-1',
+  router
+})
 
 new Vue({
   router,
