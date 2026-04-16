215336d0f93571619b7d078791e05170722c1482
AndrewGardhouse <AndrewGardhouse@users.noreply.github.com>
2026-04-16 19:32:04 +0000

    deploy: 6540308d5925e06f9026694cb5f2aca80cfb4e49

---
+++ precache-manifest.16c8b6646401a844cbb4ac23475f10f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "99f0648e6bdb583eeae1",
+    "url": "https://andrewgard.house/css/app.65f58189.css"
+  },
+  {
+    "revision": "99f0648e6bdb583eeae1",
+    "url": "https://andrewgard.house/js/app.41fe68ce.js"
+  },
+  {
+    "revision": "086476f96889c7296969",
+    "url": "https://andrewgard.house/js/chunk-vendors.3b8ff054.js"
+  },
+  {
+    "revision": "c60f19f6ad450bec33a1c54fd1b898f1",
+    "url": "https://andrewgard.house/index.html"
+  },
+  {
+    "revision": "25ae5433803545ebbd4e0e1a23c2f4ea",
+    "url": "https://andrewgard.house/google6392760f988e4702.html"
+  },
+  {
+    "revision": "5d84ae96bcac00d0ca578f92a4011fd3",
+    "url": "https://andrewgard.house/CNAME"
+  },
+  {
+    "revision": "7853a008ae5a7e33871a4d6d1363c447",
+    "url": "https://andrewgard.house/img/open-graph.png"
+  },
+  {
+    "revision": "c56b797929ae3c18fa57d3d3d93fe5d5",
+    "url": "https://andrewgard.house/robots.txt"
+  },
+  {
+    "revision": "c423d7f2485565af1344696cfb4b73b8",
+    "url": "https://andrewgard.house/sitemap.xml"
+  }
+];
